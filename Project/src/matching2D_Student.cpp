6d953b6cd1db42be4e9217fbc744ecd8234f6ea5
Suraj0712 <sssapkal@wpi.edu>
2020-06-16 01:39:14 -0400

    Task5: added FLANN bsed matcher

--- Project/src/matching2D_Student.cpp
+++ Project/src/matching2D_Student.cpp
@@ -18,7 +18,15 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
-        // ...
+        if (descSource.type() != CV_32F) 
+        {
+        	descSource.convertTo(descSource, CV_32F);
+        }
+        if (descRef.type() != CV_32F) 
+        {
+        	descRef.convertTo(descRef, CV_32F);
+        }
+        matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
     }
 
     // perform matching task
@@ -30,7 +38,6 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     else if (selectorType.compare("SEL_KNN") == 0)
     { // k nearest neighbors (k=2)
 
-        // ...
     }
 }
 
